2b0890f751a4bca4ea6fe4493889d472f96b3832
Tzvi Rolnik <tzvirol@std.hac.ac.il>
2024-11-23 22:21:16 +0200

    i wrote the first part of the main

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main()
 	auto level01 = std::ifstream("level01.txt");
 
 
-	int row_level =1, col_level =0;
+	int row_level = 1, col_level = 0;
 
 	while (level01.get() != '\n')
 		col_level++;
@@ -21,29 +21,55 @@ int main()
 	while (level01 >> c)
 	{
 		if (level01.peek() == '\n')
-			 row_level++;
+			row_level++;
 	}
 
 	cout << "col_level =" << col_level << endl
 		<< "row_level =" << row_level << endl;
-	
 
-	char i;
 
-	while (level01 >> i)
+	char** m_level = new char* [row_level]; // הקצאת שורות
+	for (int i = 0; i < row_level; i++)
 	{
-		std::cout << i;
+		m_level[i] = new char[col_level]; // הקצאת עמודות לכל שורה
+	}
 
-		if (level01.peek() == '\n')
+	level01.clear(); // מוודאים שאין שגיאות בסטרים
+	level01.seekg(0, ios::beg); // מחזירים את הקריאה להתחלה
+	
+		
+	for (int i = 0; i < row_level && !level01.eof(); i++) 
+	{
+		for (int j = 0; j < col_level && !level01.eof(); j++) 
 		{
-			std::cout << "\n";
+			level01 >> noskipws >> c;
+			if (c == '\n') 
+				j--; // דלגו על שורות ריקות
+			else 
+			  m_level[i][j] = c;
+			
+		}
+	}
+
+	
+	
+		for (int i = 0; i < row_level  ; i++)
+		{
+			for (int j = 0; j < col_level  ; j++)
+			{
+				cout <<   m_level[i][j];
+			}
+			cout << endl;
 		}
 
-		else
-			while (level01.peek() == ' ')
-			{
-				std::cout << " ";
-				level01.get();
-			}
-	}
+
+
+
+		for (int i = 0; i < row_level; i++) {
+			delete[] m_level[i]; // שחרור עמודות של כל שורה
+		}
+		delete[] m_level; // שחרור השורות
+	
+
+		return 0;
 }
